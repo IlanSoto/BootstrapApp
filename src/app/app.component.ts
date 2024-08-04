d3101cf4114bead2f08c9c0dfeef0cd9e1356c52
IlanSoto <ilan10sd@gmail.com>
2024-08-03 17:11:25 -0700

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.5s', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('0.5s', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'BootstrapApp';
+  show = true;
 }
